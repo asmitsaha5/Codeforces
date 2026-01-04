a2c8c4003cee89f6cdb1273fa3dd09a8ce56d514
asmitsaha5 <asmitsaha5@gmail.com>
2026-01-04 21:17:35 +0530

    New year stuffs

---
+++ Codeplay/1.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    cout << "Hello World" << endl;
+}
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
